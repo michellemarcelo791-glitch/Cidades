20e89a1628dd6e77e68360fa53bd9a3066f5fc2b
Michelle <michellemarcelo791@gmail.com>
2025-09-05 22:05:06 -0300

    termino do exercicio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bertioga");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Guarujá,");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+Console.WriteLine(" Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
